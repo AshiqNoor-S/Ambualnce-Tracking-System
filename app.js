fb386ac9da6b7c4f6ccfdd6101bf740a5fd14375
AshiqNoor-S <ashiqnoorsudheer@gmail.com>
2023-03-27 19:25:26 +0530

    done

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-const express = require('express');
-const app = express();
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync( 'index.html');
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+"/index.html");
-})
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
 
-app.listen(3000,function(){
-    console.log("Server is running on port 3000");
-})
+const parser = new parsers.Readline({
+  delimiter: '\r\n'
+});
+
+var port = new SerialPort('/dev/tty.wchusbserialfa1410',{ 
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false
+});
+
+port.pipe(parser);
+
+var app = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+});
+
+var io = require('socket.io').listen(app);
+
+io.on('connection', function(socket) {
+    
+  console.log('Node is listening to port');
+    
+});
+
+parser.on('data', function(data) {
+    
+  console.log('Received data from port: ' + data);
+  io.emit('data', data);
+    
+});
+
+app.listen(3000);
